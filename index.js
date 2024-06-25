6fee3c455462a5c7d438f269be88123d92b560a6
tejasshri <tejasshrishekhar08@gmail.com>
2024-06-25 15:06:48 +0530

    Descriptive commit message

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.log("Hello Sanju i AM SAI JI") ;
 console.log("Hiii I am not a developer i am a sanju") ; 
 
 console.log("Hmmmmmmm you and me") ;
+
+
+crossOriginIsolated.kggtkghefkogirekhoildhdfjgertmeen pjfgmktjgfmgjhg WebGLRenderingContext [roggokl;getComputedStyle
+    mdkkdmkm
+    nnvcjkhd jkgfnkjnfh 
+    kjlghfjg jk ndvjjjsmnfhbf fn ';lmfegklofkemfg  njfnj'
+]
